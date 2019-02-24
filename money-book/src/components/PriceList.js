cd8b37f12278dcd87dff7547cf22ee1536d69d36
js103 <643929860@qq.com>
2019-02-24 11:55:11 +0700

    引入 react-ionicons 以及使用

--- money-book/src/components/PriceList.js
+++ money-book/src/components/PriceList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Ionicon from "react-ionicons";
 
 const PriceList = ({ items, onModifyItem, onDeleteItem}) => {
     return (
@@ -17,14 +18,30 @@ const PriceList = ({ items, onModifyItem, onDeleteItem}) => {
                             {item.price}元
                         </span>
                         <span className="col-2">{item.date}</span>
-                        <button className="col-1 btn btn-primary"
-                            onClick={() => { onModifyItem(item)}}>
-                            编辑
-                        </button>
-                        <button className="col-1 btn btn-danger"
-                            onClick={() => { onDeleteItem(item) }}>
-                            删除
-                        </button>
+                        <a className="col-1"
+                            // role="button"
+                            onClick={(event) => { event.preventDefault(); onModifyItem(item) }}
+                        >
+                            <Ionicon
+                                className="rounded-circle"
+                                fontSize="30px"
+                                style={{ backgroundColor: "#28a745", padding: '5px' }}
+                                color={'#fff'}
+                                icon='ios-create-outline'
+                            />
+                        </a>
+                        <a className="col-1"
+                            // role="button"
+                            onClick={(event) => { event.preventDefault(); onDeleteItem(item) }}
+                        >
+                            <Ionicon
+                                className="rounded-circle"
+                                fontSize="30px"
+                                style={{ backgroundColor: "dc3545", padding: '5px' }}
+                                color={'#fff'}
+                                icon='ios-close'
+                            />
+                        </a>
 
                     </li>
                 ))
