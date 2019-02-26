324c1ccc99924db11d99d1d902410deb9beea10a
js103 <643929860@qq.com>
2019-02-26 15:04:54 +0700

    create.js添加

--- money-book/src/App.js
+++ money-book/src/App.js
@@ -4,15 +4,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import axios from "axios";
 import Home from "./containers/Home";
+import Create from "./containers/Create";
 
 class App extends Component {
     
   render() {
     return (
-		<div className="App">
-			<Home/>
-			
-		</div>
+		<Router>
+			<div className="App">
+				<div className="container pb-5">
+					<Route path="/" exact component={Home} />
+					<Route path="/create" component={Create} />
+					<Route path="/edit/:id" component={Create} />
+				</div>
+			</div>
+		</Router>
 	);
   }
 }
