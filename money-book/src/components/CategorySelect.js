80a1de54af958f952887643a0d74d8cd2ca4fd2f
js103 <643929860@qq.com>
2019-02-26 15:16:30 +0700

    TDD 的开发流程 1

---
+++ money-book/src/components/CategorySelect.js
@@ -0,0 +1,15 @@
+import React, { Component} from "react";
+import Ionicon from "react-ionicons";
+import PropTypes from "prop-types";
+
+
+class CategorySelect extends Component {
+
+    render() {
+        <div className="category-select-component">
+            
+        </div>
+    }
+}
+
+export default CategorySelect;
